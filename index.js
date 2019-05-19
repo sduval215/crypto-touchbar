2ebdee4874553e67b3a0ddd176f046c5a9a357ab
Steven Duval <sduval215@gmail.com>
2019-05-19 03:38:15 -0700

    abstract out currency updating request into separate function for re-usability

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const { TouchBarButton } = TouchBar;
 
 const { getCurrencyData } = require('./helpers/http');
 
+/**
+ * Fetches coinbase currency data and updates object argument label
+ */
+updateCurrencyData = async (touchBarObject) => {
+  // create request variable with coinbase BTC value requests
+  const request = await getCurrencyData('https://api.coinbase.com/v2/prices/spot?currency=USD');
+  // deconstruct necessary variables
+  const { base, currency, amount } = request.data;
+  // update toucbar label
+  touchBarObject.label = `${base} ${amount} ${currency}`;
+}
+
 app.once('ready', async () => {
 
   let window;
@@ -14,13 +26,9 @@ app.once('ready', async () => {
     height: 5,
     backgroundColor: '#000'
   })
-  // create request variable with coinbase BTC value requests
-  const request = await getCurrencyData('https://api.coinbase.com/v2/prices/spot?currency=USD');
-  // deconstruct necessary variables
-  const { base, currency, amount } = request.data;
   // set touchBarObject to new TouchBarButton class
   const touchBarObject = new TouchBarButton({
-    label: `${base} ${amount} ${currency}`,
+    label: `Loading...`,
     backgroundColor: '#F18F19',
     icon: nativeImage.createFromPath('./imgs/bitcoin-logo.png').resize({
       width: 13,
@@ -36,13 +44,9 @@ app.once('ready', async () => {
   // call window object methods with necessary information/variables
   window.loadURL('about:blank')
   window.setTouchBar(touchBar);
+  // set initial currency data
+  updateCurrencyData(touchBarObject);
+
   // once initially set, call interval every 3 minutes (180000 milliseconds)
-  setInterval(async () => {
-    // create new request variable with updated currency data
-    const request = await getCurrencyData('https://api.coinbase.com/v2/prices/spot?currency=USD');
-    // deconstruct necessary variables
-    const { base, currency, amount } = request.data;
-    // update toucbar label
-    touchBarObject.label = `${base} ${amount} ${currency}`
-  }, 180000);
+  setInterval(() => updateCurrencyData(touchBarObject), 180000);
 });
