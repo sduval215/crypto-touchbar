2614c33b611131b6c25dded74c661115f5a06a4b
Steven Duval <sduval215@gmail.com>
2019-05-19 02:18:21 -0700

    add interval logic to updatecurrency price every 3 minutes

--- index.js
+++ index.js
@@ -3,23 +3,22 @@ const { TouchBarButton } = TouchBar;
 
 const { getCurrencyData } = require('./helpers/http');
 
-let window;
-
 app.once('ready', async () => {
-  
-  // TODO: Make this prettier
+
+  let window;
+  // set window variable to new BrowserWindow class
   window = new BrowserWindow({
     frame: false,
     titleBarStyle: 'hiddenInset',
-    width: 200,
-    height: 200,
+    width: 5,
+    height: 5,
     backgroundColor: '#000'
   })
-
+  // create request variable with coinbase BTC value requests
   const request = await getCurrencyData('https://api.coinbase.com/v2/prices/spot?currency=USD');
-
+  // deconstruct necessary variables
   const { base, currency, amount } = request.data;
-
+  // set touchBarObject to new TouchBarButton class
   const touchBarObject = new TouchBarButton({
     label: `${base} ${amount} ${currency}`,
     backgroundColor: '#F18F19',
@@ -30,13 +29,20 @@ app.once('ready', async () => {
     iconPosition: 'left',
     click: () => null
   })
-  
+  // set touchbar variable to new TouchBar class
   const touchBar = new TouchBar({
-    items: [
-      touchBarObject
-    ]
+    items: [ touchBarObject ]
   });
-
+  // call window object methods with necessary information/variables
   window.loadURL('about:blank')
   window.setTouchBar(touchBar);
+  // once initially set, call interval every 3 minutes (180000 milliseconds)
+  setInterval(async () => {
+    // create new request variable with updated currency data
+    const request = await getCurrencyData('https://api.coinbase.com/v2/prices/spot?currency=USD');
+    // deconstruct necessary variables
+    const { base, currency, amount } = request.data;
+    // update toucbar label
+    touchBarObject.label = `${base} ${amount} ${currency}`
+  }, 180000);
 });
