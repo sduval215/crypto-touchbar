5d8dc0eef49403d65ab38497c789541c6dac9e4d
Steven Duval <sduval215@gmail.com>
2019-05-22 22:51:10 -0700

    finishes up ETH-LTC support + asset handling

--- helpers/touchbar.js
+++ helpers/touchbar.js
@@ -1,6 +1,12 @@
 const { TouchBar, nativeImage } = require('electron');
 const { TouchBarButton, TouchBarSpacer } = TouchBar;
 
+/**
+ * Handles icon setting through electron nativeImage object
+ * @param {string} label - image label to refer to
+ * @param {number} height - height of icon (px)
+ * @param {number} width - width of icon (px)
+ */
 const getIcon = (label = 'bitcoin', height = 16, width = 14) => {
    return nativeImage.createFromPath(`./imgs/${label}-logo.png`).resize({height, width});
 };
@@ -8,32 +14,58 @@ const getIcon = (label = 'bitcoin', height = 16, width = 14) => {
 const spacer = new TouchBarSpacer({ size: 'small '});
 
 const baseTouchBar = {
-  label: `Loading...`,
   iconPosition: 'left',
   click: () => null
 }
 
 let touchBarArray = [
   new TouchBarButton({
+    label: 'Loading...',
     backgroundColor: '#F18F19',
-    icon: getIcon('bitcoin'),
+    icon: getIcon('bitcoin', 16, 13),
     ...baseTouchBar
   }),
   spacer, // spacer element
   new TouchBarButton({
+    label: 'Loading...',
     backgroundColor: '#4C60B3',
-    icon: getIcon('bitcoin'),
+    icon: getIcon('ethereum', 18, 12),
     ...baseTouchBar
   }),
   spacer, // spacer element
   new TouchBarButton({
+    label: 'Loading..',
     backgroundColor: '#87898A',
-    icon: getIcon('bitcoin'),
+    icon: getIcon('litecoin', 14, 12),
     ...baseTouchBar
   }),
 ]
 
+/**
+ * Returns currency symbol based on background color hex string argument
+ * @param {string} backgroundColor - background color as a hex string
+ * @returns {string} - returns currency symbol string
+ */
+const getSymbolFromColor = (backgroundColor) => {
+  let currencySymbol;
+  switch(backgroundColor) {
+    case('#F18F19'): // Bitcoin
+      currencySymbol = 'BTC';
+      break;
+    case('#4C60B3'): // Ethereum
+      currencySymbol = 'ETH';
+      break;
+    case('#87898A'): // Litecoin
+      currencySymbol = 'LTC';
+      break;
+    default:
+      break;
+  }
+  return currencySymbol; //return evaluated currency symbol
+}
+
 
 module.exports = {
-  touchBarArray
+  touchBarArray,
+  getSymbolFromColor
 }
--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const { app, BrowserWindow, TouchBar } = require('electron');
 
 const { getCurrencyData } = require('./helpers/http');
-const { touchBarArray } = require('./helpers/touchbar');
+const { touchBarArray, getSymbolFromColor } = require('./helpers/touchbar');
 
 /**
  * Fetches coinbase currency data and updates object argument label
  */
-updateCurrencyData = async (touchBarObject) => {
-  // create request variable with coinbase BTC value requests
-  const request = await getCurrencyData('https://api.coinbase.com/v2/prices/spot?currency=USD');
-  // deconstruct necessary variables
-  const { base, currency, amount } = request.data;
-  // update toucbar label
-  touchBarObject.label = `${base} ${amount} ${currency}`;
+updateCurrencyData = async () => {
+  touchBarArray.forEach(async ({_label, _backgroundColor}, index) => {
+    if(_label) {
+      let currencySymbol = getSymbolFromColor(_backgroundColor);
+      const { data } = await getCurrencyData(`https://api.coinbase.com/v2/prices/${currencySymbol}-USD/spot`);
+      touchBarArray[index].label = "$" + data.amount;
+    }
+  })
 }
 
 let windowOptions = {
@@ -24,7 +25,6 @@ let windowOptions = {
   title: 'Crypto Touchbar',
 };
 
-
 app.once('ready', async () => {
   // set window variable to new BrowserWindow class
   const window = new BrowserWindow(windowOptions);
@@ -35,7 +35,7 @@ app.once('ready', async () => {
   // call window object methods with necessary information/variables
   window.setTouchBar(touchBar);
   // set initial currency data
-  updateCurrencyData(touchBarObject);
+  updateCurrencyData();
   // once initially set, call interval every 3 minutes (180000 milliseconds)
-  setInterval(() => updateCurrencyData(touchBarObject), 180000);
+  setInterval(() => updateCurrencyData(), 180000);
 });
